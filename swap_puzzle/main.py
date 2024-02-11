69c1cd92eb539abfcb3dcf4fe42b88de62ea93bc
jamevallette <156650642+jamevallette@users.noreply.github.com>
2024-02-11 23:30:44 +0100

    Ajout matplotlib

--- swap_puzzle/main.py
+++ swap_puzzle/main.py
@@ -13,7 +13,23 @@ print(file_name)
 g = Grid.grid_from_file(file_name)
 print(g)
 print(g.liste_to_condensat())
-print(tuple(5274163905634126))
+
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots(1,1)
+for i in range(0,n):
+    for j in range(0,m):
+        data = (i,j)
+        ax.axis("tight")
+        ax.axis("off")
+        ax.table(cellText=data, loc="center")
+
+
+plt.show()
+
+
+
+
 """
 from graph import Graph
 data_path = "ensae-prog24/input/"
